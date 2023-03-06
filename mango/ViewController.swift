01ec2b3e6344cb077a272b03877c9ccd45462e4e
elliottxharris <elliottxharris@gmail.com>
2023-03-05 20:49:22 -0600

    Initial Commit

---
+++ mango/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mango
+//
+//  Created by Elliott Harris on 3/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
